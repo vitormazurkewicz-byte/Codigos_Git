2b4989783f8a24a73a167f582cd1fa267af2c28d
vitormazurkewicz-byte <vitormazurkewicz@gmail.com>
2026-08-30 15:08:09 -0400

    Atividade1.java
    aprendendo java

---
+++ Atividade1.java
@@ -0,0 +1,21 @@
+algoritmo "Exercicio1"
+
+var
+   nome: caractere
+   idade: inteiro
+   altura: real
+
+inicio
+
+   escreva("Digite seu nome: ")
+   leia(nome)
+
+   escreva("Digite sua idade: ")
+   leia(idade)
+
+   escreva("Digite sua altura: ")
+   leia(altura)
+
+   escreval("Nome: ", nome, ", idade: ", idade, " anos e altura: ", altura, " metros.")
+
+fimalgoritmo
