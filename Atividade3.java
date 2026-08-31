31f421a11a716d54460227e217c1f041049e3e69
vitormazurkewicz-byte <vitormazurkewicz@gmail.com>
2026-08-31 07:37:57 -0400

    Atividade3.java

---
+++ Atividade3.java
@@ -0,0 +1,18 @@
+algoritmo "Exercicio3"
+
+var
+   reais, cotacao, dolares: real
+
+inicio
+
+   escreva("Digite o valor em reais: ")
+   leia(reais)
+
+   escreva("Digite a cotacao do dolar: ")
+   leia(cotacao)
+
+   dolares <- reais / cotacao
+
+   escreval("Valor em dolar: US$ ", dolares)
+
+fimalgoritmo
