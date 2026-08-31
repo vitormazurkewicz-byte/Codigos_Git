9213a2fe0c023418727a028dfc6c9fb1a5aea5d7
vitormazurkewicz-byte <vitormazurkewicz@gmail.com>
2026-08-31 07:38:28 -0400

    Atividade4.java

---
+++ Atividade4.java
@@ -0,0 +1,25 @@
+algoritmo "Exercicio4"
+
+var
+   nota1, nota2, nota3, nota4: real
+   media: real
+
+inicio
+
+   escreva("Digite a primeira nota: ")
+   leia(nota1)
+
+   escreva("Digite a segunda nota: ")
+   leia(nota2)
+
+   escreva("Digite a terceira nota: ")
+   leia(nota3)
+
+   escreva("Digite a quarta nota: ")
+   leia(nota4)
+
+   media <- (nota1 + nota2 + nota3 + nota4) / 4
+
+   escreval("Media final: ", media)
+
+fimalgoritmo
