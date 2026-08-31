6383dcaae3e7870d71b687820200ba613212f802
vitormazurkewicz-byte <vitormazurkewicz@gmail.com>
2026-08-31 07:37:31 -0400

    Atividade2.java

---
+++ Atividade2.java
@@ -0,0 +1,20 @@
+algoritmo "Exercicio2"
+
+var
+   num1, num2: inteiro
+
+inicio
+
+   escreva("Digite o primeiro numero: ")
+   leia(num1)
+
+   escreva("Digite o segundo numero: ")
+   leia(num2)
+
+   escreval("Soma: ", num1 + num2)
+   escreval("Subtracao: ", num1 - num2)
+   escreval("Produto: ", num1 * num2)
+   escreval("Divisao inteira: ", num1 div num2)
+   escreval("Resto da divisao: ", num1 mod num2)
+
+fimalgoritmo
