929425228118f6dc244a11a795ce31d7fc9f0545
vitormazurkewicz-byte <vitormazurkewicz@gmail.com>
2026-08-31 20:38:32 -0400

    Atividade9.java

---
+++ Atividade9.java
@@ -0,0 +1,17 @@
+algoritmo "Exercicio9"
+
+var
+   numero: inteiro
+
+inicio
+
+   escreva("Digite um numero: ")
+   leia(numero)
+
+   se numero mod 2 = 0 entao
+      escreval("O numero e PAR.")
+   senao
+      escreval("O numero e IMPAR.")
+   fimse
+
+fimalgoritmo
